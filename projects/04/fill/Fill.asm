c93961bc2d9277492bbb86b6751c526fdf74116b
Jack Draak <jackdraak@gmail.com>
2018-02-26 15:36:00 -0500

    It works! I feel it's a bit thrashy... it's contiuously setting the screen buffer to one state or the other; improvement potential: make it aware of it's own state so it only changes the screen buffer when needed, rather than at every opportunity.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,3 +1,16 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input.
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed. 
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
+
 // prepare a buffer size declaration
 	@SCREEN
 	D=A		// get address of screen buffer
@@ -19,42 +32,41 @@
 (SCAN)
 	@KBD
 	D=M		// check keyboard buffer
-	@SCAN
-	D;JEQ	// scan until input detected, else:
+	@OFF
+	D;JEQ	// off until input detected, else:
 
 (ON)
 	@pos
 	D=M
-//	@SCREEN+D // going to RAM[20]... Why?!?!
 	@SCREEN
-	A=A+D	// doing it now in 2 steps.... WORKS!
-	M=-1
+	A=A+D	// select register using @pos
+	M=-1	// set all bits in register to "black"
 	@pos
 	D=M+1	
 	M=D		// increment position register
 	@buff
 	D=D-M
-//	D=D-1	// correct the end of buffer by 1 register... didn't fix the bounds
 	@ON
 	D;JLT	// continue until finished
+	@INIT
+	0;JMP
+
+(OFF)
 	@pos
-	M=0		// bounds issue resolved by resetting @pos here (duh)
-	@SCAN
+	D=M
+	@SCREEN
+	A=A+D	// select register using @pos
+	M=0		// set all bits in register to "white"
+	@pos
+	D=M+1	
+	M=D		// increment position register
+	@buff
+	D=D-M
+	@OFF
+	D;JLT	// continue until finished
+	@INIT
 	0;JMP
 
 (END)
 	@END
 	0;JMP
-
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
