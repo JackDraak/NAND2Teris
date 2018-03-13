b66980724c2c6751bfb434257a7d89c9d7544305
Jack Draak <jackdraak@gmail.com>
2018-03-13 04:32:10 -0400

    tidying

--- Hacksemmbler/Program.cs
+++ Hacksemmbler/Program.cs
@@ -66,7 +66,7 @@ namespace Hacksemmbler
                 {
                     if (LineIsSymbolReference(instructionList[i]))
                     {
-                        debugLog.Add($"{instructionList[i]} evaluates as TRUE: LineIsSymbolReference");
+                        debugLog.Add($"{instructionList[i]}: LineIsSymbolReference");
                         if (!symbolTable.ContainsKey(instructionList[i]))
                         {
                             symbolTable.Add(instructionList[i], symbolOffset);
