afa11a8e776eff17d77ad5dac37ac62e1b40b48c
Jack Draak <jackdraak@gmail.com>
2018-03-17 02:04:16 -0400

    First (rough) tidy complete

--- Hacksemmbler/Program.cs
+++ Hacksemmbler/Program.cs
@@ -7,7 +7,7 @@ using System.Text.RegularExpressions;
 namespace Hacksemmbler
 /*  For project 6 of part I of "From Nand to Tetris", my take on the Hack Assembler: Hacksemmbler
  *  Be warned, my programming is not industrial-strength; I'm just doing this for fun and self-
- *  development. @JackDraak [this is in dire need of a tidy, but... it finally works!]
+ *  development. @JackDraak (passes unit-tests as of 16 March 2018)
  *  
  *  Primary specification reference: http://nand2tetris.org/course.php (topic: assembler)
  *  
@@ -41,22 +41,22 @@ namespace Hacksemmbler
 
         private class SymbolEntry
         {
-            string id;
-            int address;
             bool isData;
+            int address;
+            string id;
 
-            public string GetID()           { return id; }
-            public int GetAddress()         { return address; }
             public bool IsData()            { return isData; }
-            public void SetID(string str)   { id = str; }
-            public void SetAddress(int num) { address = num; }
+            public int GetAddress()         { return address; }
+            public string GetID()           { return id; }
             public void IsCode(bool torf)   { isData = !torf; }
+            public void SetAddress(int num) { address = num; }
+            public void SetID(string str)   { id = str; }
 
             public SymbolEntry()
             {
-                id = "";
-                address = 0;
                 isData = false;
+                address = 0;
+                id = "";
             }
         }
 
@@ -74,62 +74,42 @@ namespace Hacksemmbler
             while(argument < args.Length)
             {
                 // Initialize setup for each input-file in the batch.
-                String thisProgram = GetProgramName(args, argument);
                 List<string> instructionList, debugLog;
-                int nextOpenRegister;
                 List<SymbolEntry> symbolTable = new List<SymbolEntry>();
+                String thisProgram = GetProgramName(args, argument);
+                int nextOpenRegister;
                 InitEncode(out nextOpenRegister, out instructionList, out encodeHeader, out symbolTable, out debugLog);
 
                 // Pre-parse input-stream of instructions into a handy-dandy List... let's call it: instructionList.
+                // (Get rid of whitespace, including blank lines, and comments, it's for humans, not machines.)
                 PreParse(args, argument, instructionList);
 
-                // Output parsed instructions.
+                // Output parsed instructions for humans.
                 DebugPreParsed(thisProgram, true, instructionList, debugLog);
 
-                // On first-pass, build requisite symbol table. [look for label declarations, track offset]
+                // On first-pass, build requisite symbol table. 
+                // [look for label declarations, track offset]
                 BuildSymbolTable(instructionList, debugLog, symbolTable);
 
                 // Second-pass, link symbol table with variables.
                 nextOpenRegister = LinkVariables(instructionList, debugLog, nextOpenRegister, symbolTable);
 
-                // Replace variables.
-                for (int i = 0; i < instructionList.Count; i++)
-                {
-                    String labelOrNot = instructionList[i];
-                    char test = labelOrNot[0];
-                    if (test == '@')
-                    {
-                        labelOrNot = CleanAddress(labelOrNot);
-                        int addressAsInteger;
-                        bool success = int.TryParse(labelOrNot, out addressAsInteger);
-                        if (!success)
-                        {
-                            foreach (var symbolEntry in symbolTable)
-                            {
-                                if (symbolEntry.GetID() == labelOrNot)
-                                {
-                                    addressAsInteger = symbolEntry.GetAddress();
-                                    instructionList[i] = $"@{addressAsInteger}";
-                                    continue;
-                                }
-                            }
-                        }
-                    }
-                }
+                // "Third-pass", Parse variables into absulte addresses.
+                ParseVariables(instructionList, symbolTable);
 
-                // Output parsed instructions.
-                DebugPreParsed($"_{thisProgram}_", true, instructionList, debugLog);
+                // Output pre-encoded but fully parsed instructions, again for humans.
+                DebugPreParsed($"{thisProgram}-final", true, instructionList, debugLog);
 
-                // Parse and encode instructionList. 
+                // Parse and encode instructionList for machines* which are Hack-compliant. 
                 List<string> encodedInstructions = DoEncode(instructionList, debugLog, ref nextOpenRegister, symbolTable);
 
-                // Debug output: symbol table. 
+                // Debug output: dump symbol table for humans. 
                 DebugSymbols(debugLog, symbolTable, thisProgram, true);
 
-                // Output encoded data.
+                // Output encoded data for machines*.
                 string programName = OutputProgram(args, argument, encodedInstructions);
 
-                // Output debug stream.
+                // Output debug stream for humans.
                 OutputDebug(debugLog, programName);
 
                 // Chill until user hits enter or return, then exit (or continue batch).
@@ -137,63 +117,12 @@ namespace Hacksemmbler
             }
         }
 
-        private static int LinkVariables(List<string> instructionList, List<string> debugLog, int nextOpenRegister, List<SymbolEntry> symbolTable)
-        {
-            for (int i = 0; i < instructionList.Count; i++)
-            {
-                char test = instructionList[i][0];
-                if (test == '@')
-                {
-                    String labelOrNot = CleanAddress(instructionList[i]);
-                    int addressAsInteger;
-                    bool success = int.TryParse(labelOrNot, out addressAsInteger);
-                    if (!success)
-                    {
-                        // must be a symbol or variable, assign variables
-                        bool inTable = false;
-                        int thisAddress;
-                        foreach (var symbolEntry in symbolTable)
-                        {
-                            if (symbolEntry.GetID() == labelOrNot)
-                            {
-                                inTable = true;
-                                thisAddress = symbolEntry.GetAddress();
-                                Console.WriteLine($"LOOKUP: {labelOrNot}\t{thisAddress}");
-                                // what happens to thisAddress?
-                                instructionList[i] = $"@{thisAddress}";
-                                continue;
-                            }
-                        }
-                        if (!inTable & LineIsSymbolReference(labelOrNot))
-                        {
-                            // symbol, not variable
-                            Console.WriteLine("ERROR how did a symbol get here?");
-                        }
-                        else if (!inTable)
-                        {
-                            SymbolEntry thisSymbol = new SymbolEntry();
-                            Console.WriteLine($"Pass 2 VAR: {labelOrNot}\t{nextOpenRegister}");
-                            thisSymbol.SetID(labelOrNot);
-                            thisSymbol.SetAddress(nextOpenRegister);
-                            thisSymbol.IsCode(false);
-                            symbolTable.Add(thisSymbol);
-                            debugLog.Add($"variable: {labelOrNot}\t{nextOpenRegister}");
-                            //   labelOrNot = nextOpenRegister.ToString();
-                            nextOpenRegister++;
-                        }
-                    }
-                }
-            }
-
-            return nextOpenRegister;
-        }
-
         #region Internal Methods
         // \\ // \\ // \\ // \\ // \\ // \\ // \\ 
         // \\ // \\  Internal methods // \\ // \\ 
         // \\ // \\ // \\ // \\ // \\ // \\ // \\ 
 
-        // Setup symbolTable from assembly instructionList. First pass, assign symbols to symbol table, not variables
+        // Setup symbolTable from assembly instructionList. First pass, assign symbols to symbol table.
         private static void BuildSymbolTable(List<string> instructionList, List<string> debugLog,  List<SymbolEntry> symbolTable)
         {
             int symbolOffset = 0;
@@ -215,7 +144,7 @@ namespace Hacksemmbler
                     }
                     if (!inTable)
                     {
-                        // TODO strip parens, add to table, remove instruction line
+                        // Strip parens, add label to table.
                         SymbolEntry thisSymbol = new SymbolEntry();
                         thisSymbol.SetID(GetLabel(labelOrNot));
                         thisSymbol.SetAddress(symbolOffset);
@@ -224,7 +153,7 @@ namespace Hacksemmbler
                     }
                 }
                 if (!isSymbol) symbolOffset++; // NB
-                isSymbol = false;
+                isSymbol = false; // NB, amazing how helpful this line was/is.
             }
         }
 
@@ -234,7 +163,6 @@ namespace Hacksemmbler
             try { return Regex.Replace(strIn, @"@", "", RegexOptions.None, TimeSpan.FromSeconds(0.3)); }
             catch (RegexMatchTimeoutException) { return String.Empty; }
         }
-
 
         // Called after each input file is processed; eventually this will exit with a -1, 0, or 1 perhaps.
         private static int ContinueOrExit(string[] args, int argument, string programName)
@@ -265,13 +193,11 @@ namespace Hacksemmbler
                 }
                 if (outToFile)
                 {
-                    // symbol lines
                     if (LineIsSymbolReference(thisList[i]))
                     {
                         fileOut.Add($"---)\t\t{thisList[i]}");
                     }
                     else
-                    // regular lines
                     {
                         fileOut.Add($"{localOffset})\t\t{thisList[i]}");
                         localOffset++;
@@ -309,6 +235,7 @@ namespace Hacksemmbler
         }
 
         // Wrapper for encoding operation, to help keep Main tidy.
+        // TODO tidy-up this function.
         private static List<string> DoEncode(List<string> instructionList, List<string> debugLog, 
                                              ref int nextOpenRegister, List<SymbolEntry> symbolTable)
         {
@@ -330,7 +257,6 @@ namespace Hacksemmbler
                     bool success = int.TryParse(labelOrNot, out addressAsInteger);
                     if (!success)
                     {
-                        // if (symbolTable.TryGetValue(address, out int value))
                         bool inTable = false;
                         int thisAddress = 0;
                         foreach (var symbolEntry in symbolTable)
@@ -364,13 +290,12 @@ namespace Hacksemmbler
             bool success = int.TryParse(address, out addressAsInteger);
             if (!success)
             {
-                // This should never happen; addresses should have been converted from labels to numbers before encoding.
                 Console.WriteLine("FAIL: convert address to integer error: Encode16BitAddress");
             }
             int place = 0;
             int remainder = 0;
             bool resolved = false;
-            String binaryAddress = " "; // NB - return StripWhitespace(binaryAddress); The fix for Issue #3.
+            String binaryAddress = " ";
             int addressToConvert = addressAsInteger;
             // Employing "divide by 2" technique [recursive: modulo to next significant bit, until zero].
             while (!resolved)
@@ -460,7 +385,7 @@ namespace Hacksemmbler
             return se;
         }
 
-        // Convert symbol or assign label stuff
+        // Convert symbol or assign label stuff. // Depreciated?
         private static void GetSymbolOrUpdateTable(List<string> debugLog, ref int nextOpenRegister, List<SymbolEntry> symbolTable, 
                                                    int offset, List<string> fileLog, ref string labelOrNot)
         {
@@ -478,7 +403,6 @@ namespace Hacksemmbler
             if (inTable)
             {
                 string logString = $"({offset})\tConversion:\t{labelOrNot}={thisAddress}";
-                //labelOrNot = thisAddress.ToString();
                 debugLog.Add(logString);
                 fileLog.Add(logString);
             }
@@ -493,7 +417,6 @@ namespace Hacksemmbler
                 string logString = $"({offset})\tNew label:\t{labelOrNot}={nextOpenRegister}";
                 debugLog.Add(logString);
                 fileLog.Add(logString);
-                //labelOrNot = nextOpenRegister.ToString();
                 nextOpenRegister++;
             }
         }
@@ -512,9 +435,7 @@ namespace Hacksemmbler
         {
             int delimiter = strIn.IndexOf("=");
             if (delimiter > 0) return strIn.Substring(0, delimiter);
-            // NB - If there is no assignment (no = sign), return empty string.
             else return ""; 
-            //return strIn;
         }
 
         // Isolate & return jump directive.
@@ -562,6 +483,53 @@ namespace Hacksemmbler
         private static bool LineIsSymbolReference(string strIn)
         {
             return strIn.StartsWith("(") && strIn.EndsWith(")");
+        }
+
+        // Parse @variables, add into symbol table.
+        private static int LinkVariables(List<string> instructionList, List<string> debugLog, int nextOpenRegister, List<SymbolEntry> symbolTable)
+        {
+            for (int i = 0; i < instructionList.Count; i++)
+            {
+                char test = instructionList[i][0];
+                if (test == '@')
+                {
+                    String labelOrNot = CleanAddress(instructionList[i]);
+                    int addressAsInteger;
+                    bool success = int.TryParse(labelOrNot, out addressAsInteger);
+                    if (!success)
+                    {
+                        bool inTable = false;
+                        int thisAddress;
+                        foreach (var symbolEntry in symbolTable)
+                        {
+                            if (symbolEntry.GetID() == labelOrNot)
+                            {
+                                inTable = true;
+                                thisAddress = symbolEntry.GetAddress();
+                                Console.WriteLine($"LOOKUP: {labelOrNot}\t{thisAddress}");
+                                instructionList[i] = $"@{thisAddress}";
+                                continue;
+                            }
+                        }
+                        if (!inTable & LineIsSymbolReference(labelOrNot))
+                        {
+                            Console.WriteLine("ERROR how did a symbol get here?");
+                        }
+                        else if (!inTable)
+                        {
+                            SymbolEntry thisSymbol = new SymbolEntry();
+                            Console.WriteLine($"Pass 2 VAR: {labelOrNot}\t{nextOpenRegister}");
+                            thisSymbol.SetID(labelOrNot);
+                            thisSymbol.SetAddress(nextOpenRegister);
+                            thisSymbol.IsCode(false);
+                            symbolTable.Add(thisSymbol);
+                            debugLog.Add($"variable: {labelOrNot}\t{nextOpenRegister}");
+                            nextOpenRegister++;
+                        }
+                    }
+                }
+            }
+            return nextOpenRegister;
         }
 
         // Convert a List<String> into a compound string for output.
@@ -643,6 +611,34 @@ namespace Hacksemmbler
             return inName;
         }
 
+        // Parse @variables into absolute references.
+        private static void ParseVariables(List<string> instructionList, List<SymbolEntry> symbolTable)
+        {
+            for (int i = 0; i < instructionList.Count; i++)
+            {
+                String labelOrNot = instructionList[i];
+                char test = labelOrNot[0];
+                if (test == '@')
+                {
+                    labelOrNot = CleanAddress(labelOrNot);
+                    int addressAsInteger;
+                    bool success = int.TryParse(labelOrNot, out addressAsInteger);
+                    if (!success)
+                    {
+                        foreach (var symbolEntry in symbolTable)
+                        {
+                            if (symbolEntry.GetID() == labelOrNot)
+                            {
+                                addressAsInteger = symbolEntry.GetAddress();
+                                instructionList[i] = $"@{addressAsInteger}";
+                                continue;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         // Pre-parse input datastream (remove comments and whitespace).
         private static void PreParse(string[] args, int argument, List<string> instructionList)
         {
@@ -703,6 +699,7 @@ namespace Hacksemmbler
             catch (RegexMatchTimeoutException) { return String.Empty; }
         }
 
+        // Report if symbol exists.
         private static bool SymbolInTable(string labelOrNot, List<SymbolEntry> symbolTable)
         {
             for (int i = 0; i < symbolTable.Count; i++)
@@ -720,7 +717,7 @@ namespace Hacksemmbler
         // \\ // \\  Encoding tables  // \\ // \\ 
         // \\ // \\ // \\ // \\ // \\ // \\ // \\ 
 
-        // Comp encoding lookup table.
+        // Computaion-bits encoding lookup table.
         private static string EncodeComp(string strIn)
         {
             switch (strIn)
@@ -763,7 +760,7 @@ namespace Hacksemmbler
             }
         }
 
-        // Destination encoding lookup table.
+        // Destination-bits encoding lookup table.
         // TODO: target for simplification... why not handle each bit, (A, D, M), individually?
         private static string EncodeDest(string strIn)
         {
@@ -788,7 +785,7 @@ namespace Hacksemmbler
             }
         }
 
-        // Jump encoding lookup table.
+        // Jump-bits encoding lookup table.
         private static string EncodeJump(string strIn)
         {
             switch(strIn)
@@ -804,7 +801,7 @@ namespace Hacksemmbler
             }
         }
 
-        // Populate symbol table with predefined values as per specification.
+        // Populate symbol table with predefined values as per Hack specification.
         private static List<SymbolEntry> PredefineSymbols(List<SymbolEntry> symTable)
         {
             symTable.Add(EnterSymbol("SP", 0, true));
