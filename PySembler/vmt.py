8ea469f567304cc6da339cfbb8f427983e11928e
Jack Draak <jackdraak@gmail.com>
2018-04-01 21:13:17 -0400

    adding ch7 notes

--- PySembler/vmt.py
+++ PySembler/vmt.py
@@ -13,6 +13,57 @@
 #		push constant 5
 #		add
 #
+#	Additional Notes:
+#		The Stack: mapped to RAM[256..2047], pointer stored in RAM[SP]
+#		Static Segments: mapped to RAM[16..255]
+#		local, argument, this, that: RAM[2048..+], 
+#			pointer (to base) stored in RAM[]:, LCL, ARG, THIS, THAT
+#			therefore argument.7 is accessed as RAM[ARG + 7]
+
+'''	Notes from the Chapter 7 Slides available online at nand2tetris.orc (see: course)
+
+PARSER: Handles the parsing of a single .vm file, and encapsulates access to the input code. It reads VM commands, parses them, and
+provides convenient access to their components. In addition, it removes all white space and comments.
+Routine				Arguments				Returns				Function
+-------				---------				-------				--------
+Constructor			Input file / stream		--					Opens the input file/stream and gets ready to parse it.
+
+hasMoreCommands		--						boolean				Are there more commands in the input?
+
+advance				--						--					Reads the next command from the input and makes it the current command. 
+																Should be called only if hasMoreCommands is true. Initially there is no
+																current command.
+
+commandType			--						C_ARITHMETIC,		Returns the type of the current VM command. C_ARITHMETIC is returned 
+											C_PUSH, C_POP,		for all the arithmetic commands.
+											C_LABEL, C_GOTO, 
+											C_IF, C_FUNCTION,
+											C_RETURN, C_CALL
+
+arg1				--						string				Returns the first arg. of the current command. In the case of 
+																C_ARITHMETIC, the command itself (add, sub, etc.) is returned. Should
+																not	be called if the current command is C_RETURN.
+
+arg2				--						int					Returns the second argument of the current command. Should be called
+																only if the current command is C_PUSH, C_POP, C_FUNCTION, or C_CALL.
+
+CODEWRITER: Translates VM commands into Hack assembly code.
+Routine				Arguments				Returns				Function
+-------				---------				-------				--------
+Constructor			Output file / stream	--					Opens the output file/stream and gets ready to write into it.
+
+setFileName			fileName (string)		--					Informs the code writer that the translation of a new VM file is started.
+
+writeArithmetic		cmd (string)			--					Writes the assembly code that is the translation of the given arithmetic
+																command.
+
+WritePushPop		cmd (C_PUSH|C_POP),		--					Writes the assembly code that is the translation of the given command,
+					segment (string),							where command is either C_PUSH or C_POP.
+					index (int)
+
+Close				--						--					Closes the output file.
+
+'''
 import string, sys
 PLATFORM_FIRST_ARG = 1
 VERSION = "0.0.1"
